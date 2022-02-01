3fbb9fa2f40e05658613577bd4fc9c25a442a752
Иванов Иван <zaazaa@yandex.ru>
2022-02-01 18:03:43 +0300

    Показ нового окна с использованием библиотеки Prism

---
+++ PrismMVVMTestProject/ViewModels/TestViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
+using PrismMVVMTestProject.Views;
+
+
+namespace PrismMVVMTestProject.ViewModels
+{
+    class TestViewModel: BindableBase
+    {
+        public ICommand ShowCommand
+        {
+            get;
+            private set;
+        }
+        public TestViewModel()
+        {
+            ShowCommand = new DelegateCommand(ShowMethod);
+        }
+
+        private void ShowMethod()
+        {
+            PopUpWindow objPopupwindow = new PopUpWindow();
+            objPopupwindow.Show();
+        }
+    }
+}
